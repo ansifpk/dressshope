3d74908ec34b0e188d360928e9f88a106fa5c012
ansifpk <pkansif39@gmail.com>
2024-05-08 15:39:34 +0400

    commit4

--- passport.js
+++ passport.js
@@ -1,21 +1,45 @@
+// const passport = require('passport');
+// require('dotenv').config();
+// const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+// passport.serializeUser((user,done)=>{
+//     done(null,user);
+// })
+
+// passport.deserializeUser((user,done)=>{
+//     done(null,user)
+// });
+
+// passport.use(new GoogleStrategy({
+//     clientID:process.env.CLIENT_ID,
+//     clientSecret:process.env.CLIENT_SECRET,
+//     callbackURL:"http://localhost:3000/auth/google/callback",
+//     passReqToCallback:true
+// },
+// function(request , accessToken , refreshToken , profile, done){
+//  return done(null,profile);
+// }) );
 const passport = require('passport');
 require('dotenv').config();
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
-passport.serializeUser((user,done)=>{
-    done(null,user);
-})
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    throw new Error('Google OAuth2 client ID and client secret are required.');
+}
 
-passport.deserializeUser((user,done)=>{
-    done(null,user)
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
 });
 
 passport.use(new GoogleStrategy({
-    clientID:process.env.CLIENT_ID,
-    clientSecret:process.env.CLIENT_SECRET,
-    callbackURL:"http://localhost:3000/auth/google/callback",
-    passReqToCallback:true
-},
-function(request , accessToken , refreshToken , profile, done){
- return done(null,profile);
-}) );
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    passReqToCallback: true
+}, function (request, accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
